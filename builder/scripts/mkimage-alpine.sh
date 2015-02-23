66484b61f063288d2b8ea9eb2577199568a5d0cd
Andy Shinn <andys@andyshinn.as>
2015-02-22 18:17:15 -0600

    allow setting timezone in builder and set to UTC

--- builder/scripts/mkimage-alpine.sh
+++ builder/scripts/mkimage-alpine.sh
@@ -14,7 +14,7 @@ set -e
 }
 
 usage() {
-	printf >&2 '%s: [-r release] [-m mirror] [-s] [-e] [-c]\n' "$0"
+	printf >&2 '%s: [-r release] [-m mirror] [-s] [-e] [-c] [-t]\n' "$0"
 	exit 1
 }
 
@@ -39,6 +39,21 @@ mkbase() {
 		--root $ROOTFS --initdb add alpine-base
 }
 
+timezone() {
+	local timezone="${1:-UTC}"
+
+	$TMP/sbin/apk.static \
+		--repository $REPO \
+		--update-cache \
+		--allow-untrusted \
+		--root $ROOTFS \
+		--initdb add tzdata
+	cp -a "${ROOTFS}/usr/share/zoneinfo/${timezone}" "${ROOTFS}/etc/localtime"
+	$TMP/sbin/apk.static \
+		--root $ROOTFS \
+		del tzdata
+}
+
 conf() {
 	printf '%s\n' $REPO > $ROOTFS/etc/apk/repositories
 	[ $REPO_EXTRA -eq 1 ] && {
@@ -46,6 +61,7 @@ conf() {
 		printf '%s\n' "@testing $MIRROR/edge/testing" >> $ROOTFS/etc/apk/repositories
 	}
 	[ $ADD_APK_SCRIPT -eq 1 ] && cp /apk-install $ROOTFS/usr/sbin/apk-install
+	[ $ADD_TIMEZONE -eq 1 ] && timezone
 	rm -f $ROOTFS/var/cache/apk/*
 }
 
@@ -55,7 +71,7 @@ save() {
 	tar --numeric-owner -C $ROOTFS -c . | xz > rootfs.tar.xz
 }
 
-while getopts "hr:m:sec" opt; do
+while getopts "hr:m:sect" opt; do
 	case $opt in
 		r)
 			REL=$OPTARG
@@ -68,6 +84,9 @@ while getopts "hr:m:sec" opt; do
 			;;
 		e)
 			REPO_EXTRA=1
+			;;
+		t)
+			ADD_TIMEZONE=1
 			;;
 		c)
 			ADD_APK_SCRIPT=1
@@ -82,6 +101,7 @@ REL=${REL:-edge}
 MIRROR=${MIRROR:-http://nl.alpinelinux.org/alpine}
 SAVE=${SAVE:-0}
 ADD_APK_SCRIPT=${ADD_APK_SCRIPT:-0}
+ADD_TIMEZONE=${ADD_TIMEZONE:-0}
 REPO=$MIRROR/$REL/main
 REPO_EXTRA=${REPO_EXTRA:-0}
 ARCH=$(uname -m)
